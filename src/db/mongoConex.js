3dcdf31905fdcc6935be51b7d22d3e9c9e3620c7
Leandro <leandrobotali@gmail.com>
2022-06-28 21:25:39 -0300

    cuarto commit

--- src/db/mongoConex.js
+++ src/db/mongoConex.js
@@ -5,11 +5,9 @@ const { MONGODB_USER, MONGODB_PASS, MONGODB_HOST, MONGODB_DATABASE } = process.e
 
 const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_HOST}/${MONGODB_DATABASE}`
 
-export default mongoConex = async()=>{ 
-    await mongoose.connect(MONGODB_URI)
-    .then(db => logger.info('database is connected'))
-    .catch(err => {
+export default mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(db => logger.info('database is connected'))
+.catch(err => {
     logger.error('connection error')
     process.exit(1)
-})
-}
+});
